3c19da48237c90621c32645599186443d96cf862
dianMcdawn <patricio.a.henriquez>
2024-11-06 10:07:31 -0300

    commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Getting variable data
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        //Console.WriteLine("Hello Prep1 World!");
+        //New Line
+        Console.WriteLine("");
+        //Printing result
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Getting variable data
+        Console.Write("Please, enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        float gradePerc = float.Parse(grade);
+        string letter;
+        string passed;
+
+        if (gradePerc >= 90) { letter = "A"; }
+        else if (gradePerc >= 80) { letter = "B"; }
+        else if (gradePerc >= 70) { letter = "C"; }
+        else if (gradePerc >= 60) { letter = "D"; }
+        else if (gradePerc < 60) { letter = "F"; }
+        else { letter = "No grade"; }
+
+        Console.Write("");
+        Console.Write($"Your grade is: {letter}");
+
+        if (letter == "A" || letter == "B" || letter == "C") { passed = "Approved"; }
+        else { passed = "Fail"; }
+
+        Console.Write("");
+        Console.Write($"You have {passed} this course.");
+
         Console.WriteLine("Hello Prep2 World!");
     }
 }
