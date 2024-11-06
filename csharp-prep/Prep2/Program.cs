bdc680a2526ea0ca8fc0febb9aac5d1af16dcb1d
dianMcdawn <patricio.a.henriquez>
2024-11-06 10:32:37 -0300

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -9,6 +9,7 @@ class Program
         string grade = Console.ReadLine();
         float gradePerc = float.Parse(grade);
         string letter;
+        string symbol;
         string passed;
 
         if (gradePerc >= 90) { letter = "A"; }
@@ -18,15 +19,19 @@ class Program
         else if (gradePerc < 60) { letter = "F"; }
         else { letter = "No grade"; }
 
-        Console.Write("");
-        Console.Write($"Your grade is: {letter}");
+        if (gradePerc < 90 && gradePerc >= 87 || gradePerc < 80 && gradePerc >= 87 || gradePerc < 70 && gradePerc >= 77 || gradePerc < 60 && gradePerc >= 67) { symbol = "+"; }
+        else if (gradePerc < 93 && gradePerc > 90 || gradePerc < 83 && gradePerc > 80 || gradePerc < 73 && gradePerc > 70 || gradePerc < 63 && gradePerc > 60) { symbol = "-"; }
+        else { symbol = ""; }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your grade is: {letter}{symbol}");
 
         if (letter == "A" || letter == "B" || letter == "C") { passed = "Approved"; }
         else { passed = "Fail"; }
 
-        Console.Write("");
-        Console.Write($"You have {passed} this course.");
+        Console.WriteLine("");
+        Console.WriteLine($"You have {passed} this course.");
 
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
     }
 }
