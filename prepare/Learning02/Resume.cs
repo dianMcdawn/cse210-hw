e8ba32ae9463281eb5c14eccbf73e544e4857786
dianMcdawn <patricio.a.henriquez>
2024-11-11 11:04:41 -0300

    push

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job j in _jobs)
+        {
+            Console.WriteLine($"{j._jobTitle} ({j._company}) {j._startYear}-{j._endYear}");
+        }
+    }
+}
