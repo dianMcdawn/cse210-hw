fbdad9884f478c9f16e5d2169f903ec13b95dc35
dianMcdawn <patricio.a.henriquez>
2024-11-18 10:46:00 -0300

    sync

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Console.WriteLine("Hello Learning03 World!");
+
+        Console.WriteLine("Testing fractions encapsulations");
+        Console.WriteLine("");
+
+        //Crating new fraction
+        Fraction fraction = new Fraction();
+        fraction.SetTop(1);
+        fraction.SetBottom(3);
+
+        //Showing results
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
