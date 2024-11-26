0ea7d603dd06701654e71dd2ba97c0933d901aa3
dianMcdawn <patricio.a.henriquez>
2024-11-26 12:20:48 -0300

    commit

--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -2,38 +2,39 @@ public class Video
 {
     private string _title;
     private string _author;
-    private string _lenght; //Length in seconds
+    private int _length; //Length in seconds
     private List<Comment> _comments = new List<Comment>();
 
     //Constructors
     public Video()
     {
-        _title="";
-        _author="",
-        _length=0;
+        _title = "";
+        _author = "";
+        _length = 0;
     }
     public Video(string title)
     {
-        _title=title;
-        _author="",
-        _length=0;
+        _title = title;
+        _author = "";
+        _length = 0;
     }
     public Video(string title, string author)
     {
-        _title=title;
-        _author=author,
-        _length=0;
+        _title = title;
+        _author = author;
+        _length = 0;
     }
     public Video(string title, string author, int seconds)
     {
-        _title=title;
-        _author=author,
-        _length=seconds;
+        _title = title;
+        _author = author;
+        _length = seconds;
     }
 
     //Setters
-    public void SetNewComment(string name, string comment){
-        Comment newcom = new Comment(name,comment);
+    public void SetNewComment(string name, string comment)
+    {
+        Comment newcom = new Comment(name, comment);
         _comments.Add(newcom);
     }
 
@@ -41,10 +42,10 @@ public class Video
     //Methods
     public void Display()
     {
-        Console.WriteLine($"Video Title: {_title} - Author: {_author} - Lenght: {_lenght}sec");
+        Console.WriteLine($"Video Title: {_title} - Author: {_author} - Lenght: {_length}sec");
         int numberComments = ShowCommentQuantity();
         Console.WriteLine($"Number of comments: {numberComments}");
-        foreach(comments as c in _comments)
+        foreach (Comment c in _comments)
         {
             c.Display();
         }
@@ -52,11 +53,7 @@ public class Video
 
     private int ShowCommentQuantity()
     {
-        int count = 0;
-        foreach(comments as c in _comments)
-        {
-            count++;
-        }
+        int count = _comments.Count;
         return count;
     }
 }
